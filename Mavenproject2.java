9438053e58f94c4650adca83019ea606254d4fb9
Dangquyet2708 <dvbq2006@gmail.com>
2026-01-07 15:45:34 +0700

    Create Mavenproject2.java

---
+++ Mavenproject2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+public class Mavenproject2 {
+    public static void main(String[] args) {
+        for (int i=0;i<11;i++){
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
